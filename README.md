7b1008d5c8f4af2a4463a02c8d28faddab6e5127
wangxian <xian366@126.com>
2016-12-01 23:44:40 +0800

    修改：完成dispatch

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # ePHP - a PHP micro framework
+
+## 使用composer创建项目
+composer create-project wangxian/ephp-installer myapp
--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -1,11 +1,22 @@
 <?php
 namespace App\Controllers;
 
+use App\Services\DemoService;
+
 class IndexController
 {
 
-    function __construct()
+    function test()
     {
-        echo time();
+        dump("SERVER内容", $_SERVER);
+        dump("系统信息", $_GET, $_POST);
+
+        // echo time();
+        run_info();
+
+        (new DemoService())->hello();
+        // 1 / 0;
+        // echo $name;
+        // echo 120;
     }
 }
