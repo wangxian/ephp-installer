2042d0917e4c4ea54aa24338b8ff659465a75295
wangxian <xian366@126.com>
2017-01-14 20:23:50 +0800

    fix docker config

--- docker/startup.sh
+++ docker/startup.sh
@@ -5,8 +5,8 @@ echo "192.168.1.106 api.xxx.com" >> /etc/hosts
 
 sh -c 'printf 5000 > /net/core/somaxconn'
 
-chmod 777 -R app/logs/
-chmod 777 -R app/cache/
+chmod 777 -R logs/
+chmod 777 -R cache/
 
-exec php-fpm -F &
+exec php-fpm7 -F &
 exec nginx
