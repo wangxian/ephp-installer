5cd278bcb6a81253505bfccadc94dac6061681db
wangxian <xian366@126.com>
2017-03-04 11:38:25 +0800

    修改：测试

--- app/Controllers/IndexController.php
+++ app/Controllers/IndexController.php
@@ -9,39 +9,13 @@ class IndexController extends RootController
 
     function index()
     {
-        show_404();
-        // echo 123;
+        // show_404();
         // throw new \ePHP\Exception\ExitException('11');
         // dump($this->model->table('mytest')->data(['name'=>time()])->insert());
         // dump($this->model->table('mytest')->where(['id'=>22])->findAll());
         // dump($this->model->query('show tables')->find());
         // echo '123';
         // run_info();
-        return false;
-        // $q = new \SplQueue();
-        // $q->setIteratorMode(\SplQueue::IT_MODE_DELETE);
-        // $q[] = time();
-        // $q[] = time();
-        // $q[] = time();
-        // var_dump($q);
-
-        // return false;
-        $x = \App\Services\DemoService::init();
-        $x->pool->enqueue((object)[$x->num++]);
-        var_dump($x->pool);
-        // array_push($x->pool, new \mysqli('127.0.0.1', 'root', '111111', 'test'));
-        // foreach ($x->pool as $v)
-        // {
-        //     dump($v->thread_id);
-        // }
-        // ob_start();
-        // $res = (new \ePHP\Model\BaseModel())->table('test')->where(["amount"=>22])->findAll();
-        // dump($res);
-        // $db = new \mysqli('127.0.0.1', 'root', '111111', 'test');
-        // var_dump($db->query('select * from test'));
-        // echo '123';
-        // $this->response->end(ob_get_clean());
-        // echo 'hello';
 
 
         // return ;
@@ -69,10 +43,6 @@ class IndexController extends RootController
         // echo $name;
         // echo 120;
 
-        // var_dump($this->view);
-        // dump($this->infos);
-
-        $this->view->infos = $this->infos;
         $this->view->name  = 'ePHP';
         $this->view->render();
     }
