cbfc317eeb3a2bb39e2f2259fcb81585ebb7e150
wangxian <xian366@126.com>
2018-11-19 18:57:16 +0800

    修改：测试swoole_table

--- tests/table.php
+++ tests/table.php
@@ -1,16 +1,17 @@
 <?php
 // 测试 swoole table
 use Swoole\Table;
-$table = new \Swoole\Table(112048);
+$table = new \Swoole\Table(2048);
 
-$table->column('id',Table::TYPE_INT, 1);
-$table->column('data',Table::TYPE_STRING, 50);
+$table->column('id', Table::TYPE_INT, 1);
+$table->column('data', Table::TYPE_STRING, 50);
 $table->create();
 
-for($i=0; $i<1300; $i++) {
+for($i=0; $i<1253; $i++) {
+    echo $i . " = 进行时...\n";
     $table->set("c". $i, ["id"=> $i, "data"=> "ok-". $i]);
 }
 
 var_dump($table->get("c5"));
 echo 'pid='. getmypid();
-sleep(20);
+// sleep(20);
